04112077a155e0a1b1368f4f1c7161fab596cf76
Ashwini <shettyashwini0328@gmail.com>
2025-03-23 07:18:44 +0000

    Code challenge submission for question: 2239 Find Closest Number to Zero

---
+++ 2239 Find Closest Number to Zero/2239find-closest-number-to-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int close=nums[0];
+        for(int num:nums)
+        {
+            if(Math.abs(num)<Math.abs(close) || Math.abs(num)==Math.abs(close) && num>close)
+                close=num;
+        }
+        return close;
+    }
+}
