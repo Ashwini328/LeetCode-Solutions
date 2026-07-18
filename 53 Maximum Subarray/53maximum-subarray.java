3b2afc561bf2ab77b94089c69621e8ccf45fda2e
Ashwini <shettyashwini0328@gmail.com>
2026-07-18 08:39:09 +0000

    Code challenge submission for question: 53 Maximum Subarray

---
+++ 53 Maximum Subarray/53maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = nums[0];
+        if (nums.length > 1) {
+            for (int num : nums) {
+                sum += num;
+                max = Math.max(sum, max);
+                if (sum < 0)
+                    sum = 0;
+            }
+        }
+        return max;
+    }
+}
